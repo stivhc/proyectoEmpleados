00ce8e78b32bfd93ca3c40b53a39b1c3623cc758
stiv.hc <esteban.alfonso999@gmail.com>
2024-10-14 22:42:07 -0300

    creamos vista

--- empleados/views.py
+++ empleados/views.py
@@ -1,3 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+
+def mostrar_empleados(request):
+    empleados = [
+        "Ramírez Mantequilla",
+        "Fideo Fidel",
+        "Carlitos Lechuga",
+        "Mario Hugo",
+        "Juan Carlos Bodoque",
+        "Tulio Tribiño",
+    ]
+    context = {"empleados": empleados}
+    return render(
+        request, "empleados/mostrar_empleados.html", context
+    )  # Create your views here.
